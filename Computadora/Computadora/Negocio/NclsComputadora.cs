df224908e0bb096979ad1b81e436e172cde3e925
Mariarivas <87671658+maryrivas01@users.noreply.github.com>
2021-08-16 15:13:47 -0600

    Computadora

--- Computadora/Computadora/Negocio/NclsComputadora.cs
+++ Computadora/Computadora/Negocio/NclsComputadora.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Computadora.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,21 @@ namespace Computadora.Negocio
 {
     class NclsComputadora
     {
-        
-
+        public string Saludo(Clscomputadora computadora)
+        {
+            return "Bienvenido querido usuario";
+        }
+        public string Reiniciar(Clscomputadora computadora)
+        {
+            return "Reiniciando computadora";
+        }
+        public string Apagar(NclsComputadora computadora)
+        {
+            return "Apagando computadora";
+        }
+        public string Encender(Clscomputadora computadora)
+        {
+            return "La memoria Ram de su Pc con capacidad de" + computadora.Capacidad_ram + " GB y micro procesadora " + computadora.Procesador + " Está encendida";
+        }
     }
 }
